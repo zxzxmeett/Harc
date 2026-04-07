2ceda2ca6ddd32690399a2e29d3d82c1f810978b
zxzxmeett <meet0201parmar@gmail.com>
2026-04-07 17:59:29 +0530

    Xp count with levels

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 
+const users = {};
+
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 client.once("clientReady", () => {
-  console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", (message) => {
     if (message.author.bot) return;
-    
-    console.log("User:", message.content);
-    
-    //message.reply("Got your message");
+
+    const userId = message.author.id;
+    //If user not exists in the users object, create a new entry for them
+    if (!users[userId]) {
+    users[userId] = {
+            xp: 0,
+            level: 1
+        };
+    }
+    //give XP
+    users[userId].xp += 10;
+
+    const user = users[userId];
+
+    // check level up
+    const xpNeeded = user.level * 100;
+    if (user.xp >= xpNeeded) {
+        user.level++;
+        user.xp = user.xp - xpNeeded;
+
+        console.log(`LEVEL UP → ${message.author.username} is now level ${user.level}`);
+    }
+    console.log(users[userId]);
 });
 
 client.login(process.env.TOKEN);
